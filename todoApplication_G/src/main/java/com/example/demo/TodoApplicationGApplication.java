9930b8a4a6ddfd51274da279087eee94ce33ceb3
anjajoo <anand.jajoo@daimler.com>
2017-09-15 14:23:20 +0530

    Committed

---
+++ todoApplication_G/src/main/java/com/example/demo/TodoApplicationGApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplicationGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplicationGApplication.class, args);
+	}
+}
